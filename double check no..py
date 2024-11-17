1284011814259dd1cbf43577e1254ae183f07d81
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-16 21:46:20 -0800

    vgrbghjb m   mng bb v hgbfb fgvfv

---
+++ double check no..py
@@ -0,0 +1,11 @@
+number=int(input("enter no. to check"))
+if number>=50:
+    print ("the no. is greater than 50")
+    if number%2==0:
+          print("and it is even no.")
+    else:
+         print("and it is odd no.")
+else:
+     print("number is less than 50")
+     
+    
